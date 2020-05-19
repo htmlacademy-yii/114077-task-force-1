ee72efca6142b3484c7af6993e686e6638fcb10d
Иван Загайнов <fohunoff@gmail.com>
2020-05-19 13:41:41 +0300

    [feat] драфт схемы (внешние ключи)

--- db.sql
+++ db.sql
@@ -1,23 +1,22 @@
 CREATE TABLE `user`
 (
-    `id`            INT(11)      NOT NULL AUTO_INCREMENT,
-    `email`         varchar(255) NOT NULL,
-    `password`      varchar(255) NOT NULL,
-    `reg_date`      DATETIME     NOT NULL,
-    `last_active`   DATETIME     NOT NULL,
-    `name`          varchar(255) NOT NULL,
-    `last_name`     varchar(255) NOT NULL,
-    `patronym_name` varchar(255),
+    `id`            INT(11)  NOT NULL AUTO_INCREMENT,
+    `email`         TEXT     NOT NULL,
+    `password`      TEXT     NOT NULL,
+    `reg_date`      DATETIME NOT NULL,
+    `last_active`   DATETIME NOT NULL,
+    `name`          TEXT     NOT NULL,
+    `last_name`     TEXT     NOT NULL,
+    `patronym_name` TEXT,
     `birthday`      DATE,
     `bio`           TEXT,
-    `avatar_path`   varchar(255) NOT NULL DEFAULT 'null',
-    `rating`        INT(11),
-    `viewing_count` INT(11)      NOT NULL DEFAULT '0',
-    `role`          INT(11)      NOT NULL,
+    `avatar_path`   TEXT,
+    `viewing_count` INT(11)  NOT NULL DEFAULT '0',
+    `role`          ENUM(''),
     `phone`         varchar(11),
     `skype`         varchar(255),
     `telegram`      varchar(255),
-    `user_status`   INT(255)     NOT NULL,
+    `user_status`   ENUM(''),
     PRIMARY KEY (`id`)
 );
 
@@ -30,20 +29,20 @@ CREATE TABLE `user_role`
 
 CREATE TABLE `task`
 (
-    `id`              INT(11)      NOT NULL AUTO_INCREMENT,
-    `publish_date`    DATETIME     NOT NULL,
-    `status_id`       INT(11)      NOT NULL,
-    `title`           varchar(255) NOT NULL,
-    `description`     TEXT         NOT NULL,
-    `budget`          INT(11)      NOT NULL,
-    `customer`        INT(11)      NOT NULL,
-    `category_id`     INT(11)      NOT NULL,
-    `latitude`        varchar(255),
-    `longitude`       varchar(255),
+    `id`              INT(11)  NOT NULL AUTO_INCREMENT,
+    `publish_date`    DATETIME NOT NULL,
+    `status_id`       ENUM(''),
+    `title`           TEXT     NOT NULL,
+    `description`     TEXT     NOT NULL,
+    `budget`          INT(11),
+    `customer_id`     INT(11)  NOT NULL,
+    `category_id`     INT(11)  NOT NULL,
+    `latitude`        FLOAT,
+    `longitude`       FLOAT,
     `city_id`         INT(11),
-    `enforcer`        INT(11),
+    `enforcer_id`     INT(11),
     `additional_text` TEXT,
-    `messages`        INT          NOT NULL,
+    `messages`        INT      NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -63,8 +62,10 @@ CREATE TABLE `task_category`
 
 CREATE TABLE `city`
 (
-    `id`   INT(11)      NOT NULL AUTO_INCREMENT,
-    `name` varchar(255) NOT NULL,
+    `id`        INT(11)      NOT NULL AUTO_INCREMENT,
+    `name`      varchar(255) NOT NULL,
+    `latitude`  FLOAT        NOT NULL,
+    `longitude` FLOAT        NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -100,18 +101,11 @@ CREATE TABLE `photo`
 
 CREATE TABLE `messages`
 (
-    `id`                INT(11) NOT NULL AUTO_INCREMENT,
-    `sender_id`         INT(11) NOT NULL,
-    `recipient_id`      INT(11) NOT NULL,
-    `message`           TEXT    NOT NULL,
-    `message_status_id` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)
-);
-
-CREATE TABLE `message_status`
-(
-    `id`     INT(11)     NOT NULL AUTO_INCREMENT,
-    `status` varchar(11) NOT NULL,
+    `id`           INT(11) NOT NULL AUTO_INCREMENT,
+    `sender_id`    INT(11) NOT NULL,
+    `recipient_id` INT(11) NOT NULL,
+    `message`      TEXT    NOT NULL,
+    `status`       INT     NOT NULL,
     PRIMARY KEY (`id`)
 );
 
@@ -132,62 +126,59 @@ CREATE TABLE `category`
 CREATE TABLE `user_category`
 (
     `user_id`     INT(11) NOT NULL,
-    `categody_id` INT(11) NOT NULL
+    `category_id` INT(11) NOT NULL
 );
 
 ALTER TABLE `user`
-    ADD FOREIGN KEY (`role`) REFERENCES `user_role` (`id`);
+    ADD CONSTRAINT `user_role_fk` FOREIGN KEY (`role`) REFERENCES `user_role` (`id`);
 
 ALTER TABLE `user`
-    ADD FOREIGN KEY (`user_status`) REFERENCES `user_status` (`id`);
+    ADD CONSTRAINT `user_status_fk` FOREIGN KEY (`user_status`) REFERENCES `user_status` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`status_id`) REFERENCES `task_status` (`id`);
+    ADD CONSTRAINT `task_status_fk` FOREIGN KEY (`status_id`) REFERENCES `task_status` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`customer`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `customer_id_fk` FOREIGN KEY (`customer_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`category_id`) REFERENCES `task_category` (`id`);
+    ADD CONSTRAINT `task_category_fk` FOREIGN KEY (`category_id`) REFERENCES `task_category` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
+    ADD CONSTRAINT `city_fk` FOREIGN KEY (`city_id`) REFERENCES `city` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`enforcer`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `enforcer_id_fk` FOREIGN KEY (`enforcer_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `task`
-    ADD FOREIGN KEY (`messages`) REFERENCES `messages` (`id`);
+    ADD CONSTRAINT `task_messages_fk` FOREIGN KEY (`messages`) REFERENCES `messages` (`id`);
 
 ALTER TABLE `task_response`
-    ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+    ADD CONSTRAINT `task_task_response_fk` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
 
 ALTER TABLE `task_response`
-    ADD FOREIGN KEY (`enfoncer_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `user_task_response_fk` FOREIGN KEY (`enfoncer_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user_review`
-    ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `user_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user_review`
-    ADD FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
+    ADD CONSTRAINT `task_user_review_fk` FOREIGN KEY (`task_id`) REFERENCES `task` (`id`);
 
 ALTER TABLE `user_review`
-    ADD FOREIGN KEY (`reviewer_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `user_reviewer_id_fk` FOREIGN KEY (`reviewer_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `photo`
-    ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `photo_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `messages`
-    ADD FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `messages_sender_id_fk` FOREIGN KEY (`sender_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `messages`
-    ADD FOREIGN KEY (`recipient_id`) REFERENCES `user` (`id`);
-
-ALTER TABLE `messages`
-    ADD FOREIGN KEY (`message_status_id`) REFERENCES `message_status` (`id`);
+    ADD CONSTRAINT `messages_recipient_id_fk` FOREIGN KEY (`recipient_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user_category`
-    ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
+    ADD CONSTRAINT `user_category_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`);
 
 ALTER TABLE `user_category`
-    ADD CONSTRAINT `user_category_fk1` FOREIGN KEY (`categody_id`) REFERENCES `category` (`id`);
+    ADD CONSTRAINT `user_category_category_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`);
